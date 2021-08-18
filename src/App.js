ad23dd4ad81c14ed634fe3cca12110e31e41deda
brachiKaniel <brachi@leader.codes>
2021-08-18 14:12:54 +0300

    changes2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Exe1 from './Exe1'
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Exe1></Exe1>
         <a
           className="App-link"
           href="https://reactjs.org"
